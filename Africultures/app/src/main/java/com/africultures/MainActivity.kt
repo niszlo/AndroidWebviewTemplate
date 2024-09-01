7cedfb7f3e21469b8aba8d46f0caadf65c7d03d5
niszlo <nsloz@hotmail.com>
2024-09-01 19:57:47 +0200

    Finalizing export with icons and renaming

---
+++ Africultures/app/src/main/java/com/africultures/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.africultures
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.window.OnBackInvokedDispatcher
+import com.africultures.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+    // you can make offline application with local file
+    //private val applicationUrl = "file:///android_asset/index.html"
+    //or you can load url
+    //private val applicationUrl ="https://www.programmerbee.com/"
+    private val applicationUrl ="https://pwa.africultures.com"
+    //private val applicationUrl = "https://www.wikipedia.org/"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView = binding.webView
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT
+            ) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        }
+
+
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+
+
+        webView.loadUrl(applicationUrl)
+    }
+
+}
